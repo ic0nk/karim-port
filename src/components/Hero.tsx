33064f332dff784fd80b905f5b2d6c18e8f355b7
wael.iconk@gmail.com <wael.iconk@gmail.co>
2025-10-11 16:19:45 +0300

    feat: update Header and Hero components with new styles and buttons

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -3,11 +3,16 @@ import Image from "next/image";
 const Hero = () => {
   return (
     <section className="flex flex-col justify-between h-lvh">
-      <div className="justify-self-start text-center mt-20">
+      <div className="justify-self-start text-center mt-40">
         <h1>PORTFOLIO</h1>
         <h4 className="portfolio-subtitle">KARIM MASSAOUD</h4>
-        <div>
-
+        <div className="flex justify-center gap-4 mt-8">
+          <button className="btn btn-primary">
+            My Projects
+          </button>
+          <button className="btn btn-secondary">
+            Contact Me
+          </button>
         </div>
       </div>
       <div className="w-full">
